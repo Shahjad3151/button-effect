a3fff4bff6c7449c8f9b2e95fd01f9098c2580ce
Md Shahjad Ansari <119397040+Shahjad3151@users.noreply.github.com>
2024-07-12 08:57:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+function fun1(){
+    var n= document.getElementById("t1").value;
+    document.getElementById("t13").style.backgroundColor='red';
+}
+function fun2(){
+    var n= document.getElementById("t2").value;
+    document.getElementById("t13").style.backgroundColor='green';
+}
+function fun3(){
+    var n= document.getElementById("t3").value;
+    document.getElementById("t13").style.backgroundColor='yellow';
+}
+function fun4(){
+    var n= document.getElementById("t4").value;
+    document.getElementById("t13").style.backgroundColor='blue';
+}
+function fun5(){
+    var n= document.getElementById("t5").value;
+    document.getElementById("i1").src="image2.jpg";
+}
+function fun6(){
+    var n= document.getElementById("t6").value;
+    document.getElementById("i1").src="image3.jpg";
+}
+function fun7(){
+    var n= document.getElementById("t7").value;
+    document.getElementById("i1").src="image4.jpg";
+}
+function fun8(){
+    var n= document.getElementById("t8").value;
+    document.getElementById("i1").src="image1.jpg";
+}
+function fun9(){
+    var n= document.getElementById("t9").value;
+    document.getElementById("i2").src="image3.jpg";
+}
+function fun10(){
+    var n= document.getElementById("t10").value;
+    document.getElementById("i2").src="image1.jpg";
+}
+function fun11(){
+    var n= document.getElementById("t11").value;
+    document.getElementById("i2").src="image4.jpg";
+}
+function fun12(){
+    var n= document.getElementById("t12").value;
+    document.getElementById("i2").src="image2.jpg";
+}
